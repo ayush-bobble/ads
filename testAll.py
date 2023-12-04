b1f1eeb7b362eeb0ab3b757c7756db536dad86eb
ayush-bobble <140396786+ayush-bobble@users.noreply.github.com>
2023-12-04 14:43:01 +0530

    Update testAll.py

--- testAll.py
+++ testAll.py
@@ -15,7 +15,7 @@ def get_title_and_response(click_urls):
             # Open URL in a headless browser and get page title
             options = webdriver.ChromeOptions()
            # options.headless = True
-            options.add_argument('--headless')
+           # options.add_argument('--headless')
             browser = webdriver.Chrome(options=options)
             browser.get(url)
             page_title = browser.title
