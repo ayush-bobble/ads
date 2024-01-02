03ee25a5e43803a8bceb6ee21d54bdbc7f9a4828
ayush-bobble <140396786+ayush-bobble@users.noreply.github.com>
2024-01-02 13:32:27 +0530

    Update testAll.py

--- testAll.py
+++ testAll.py
@@ -7,9 +7,10 @@ def get_title_and_response(click_urls):
     # Create Chrome browser with options
     options = webdriver.ChromeOptions()
     options.headless = True  # Set to True if you want to run in headless mode
+    chrome_driver_path = "/usr/lib/chromium-browser/"
 
     # Start Chrome browser
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome(executable_path=chrome_driver_path ,options=options)
 
     # Initialize lists to store results
     page_titles = []
