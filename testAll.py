2f1b8bab0c839a80bafb62fb55ddd3104b2dd69f
uttam-sin <uttam.singh@bobble.ai>
2024-01-03 11:14:00 +0530

    minor change

--- testAll.py
+++ testAll.py
@@ -12,7 +12,7 @@ def get_title_and_response(click_urls):
     # chrome_driver_path = Service("/usr/lib/chromium-browser/chromedriver")
     # chrome_driver_path = Service("/usr/bin/chromedriver")
     # firefox_driver_path = FirefoxService("/snap/bin/geckodriver")
-    firefox_driver_path = "/usr/lib/chromium-browser/chromedriver"
+    firefox_driver_path = r"/usr/bin/chromedriver"
 
     # options = webdriver.ChromeOptions()
     options = webdriver.ChromeOptions()
